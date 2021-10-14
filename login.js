d3df456012274c9ef6af1fd4006f84949d37e995
Marco Gonzalez <marco.g.gleztorres@gmail.com>
2021-10-14 17:27:59 -0500

    Actualizacion en script
    se modifico el script para que funcione el redireccionamiento

--- login.js
+++ login.js
@@ -49,11 +49,11 @@ function crear(){
       
       // alert que se creo la cuenta y se dirije a otra pagina 
       alert("You have sucesfully created a account");
-      window.location.href ="./loginOwner.html";
       
       // se incrementa el contador  de usuarios y se actualiza 
       i++;
       localStorage.setItem("i", i);
+      window.location.href ="./loginOwner.html";
 
     }
   }
