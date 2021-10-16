f7de565320d224059d55a5edd2501b63ca84f462
Marco Gonzalez <marco.g.gleztorres@gmail.com>
2021-10-16 17:49:28 -0500

    Encriptacion
    Se agrego encriptación de la contraseña

--- scripts/script-login-local.js
+++ scripts/script-login-local.js
@@ -1,4 +1,5 @@
-//Evento para inicializar el evento de registro de usuarios
+//Evento para inicializar el evento de registro de usuarios1
+
 document.getElementById("sign-up-btn").addEventListener("click", registerUser);
 
 //Funcion de registro de usuario
@@ -15,13 +16,14 @@ function registerUser() {
     } if (password1.length < 8) {
         alert("Password must have at least 8 characters");
     } else {
-        // variable que sirve para saber si un correo esta dado de alta
-        var correo = false;
+        //Encriptacion de la contrasena
+        password1 = btoa(password1);
         // objeto con los datos que se obtuvieron del registerUser
         var user = new Object();
         user.name = name;
         user.mail = email;
         user.password = password1;
+        user.fullname = fullName;
         //Guarda las variables en formato JSON en una nueva variable llamada local
         var local = JSON.stringify(user);
         localStorage.setItem(`user data`, local);
@@ -38,15 +40,16 @@ document.getElementById("sign-in-btn").addEventListener("click", validate);
 function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
+password = btoa(password);
 let usuario = JSON.parse(localStorage.getItem(`user data`));
-if ( username == usuario.name && password == usuario.password){ 
+if (username == usuario.name && password == usuario.password){ 
 alert ("You sign in successfully");
 //window.location = "success.html"; // Redirecting to other page.
 return false;
 }
 else{
 attempt --;// Decrementa en uno la cantidad de intentos
-alert("You have left "+attempt+" attempt;");
+alert("You have " + attempt +" attempts left");
 // Desabilita los campos tras tres intentos fallidos de login
 if( attempt == 0){
 document.getElementById("username").disabled = true;
