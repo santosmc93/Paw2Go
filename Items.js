2d5b1082539c383aa434367dc3a81403b32fa3c5
santosmc93 <carlos.santosm93@gmail.com>
2021-10-27 23:28:27 -0500

    POST & GET
    se agregan los fetch de post y get a la pagina de objetos

--- Items.js
+++ Items.js
@@ -1,12 +1,13 @@
 class Articulo {
-    constructor(nombre, descripcion, imagen, precio, categorias = [], gato, perro){
-        this.nombre = nombre;
-        this.descripcion = descripcion;
-        this.imagen = imagen;
-        this.precio = precio;
-        this.categorias = categorias;
-        this.gato = gato;
-        this.perro = perro;
+    constructor(product_name, product_description, url_image, product_price, for_cat, for_dog, id_product, product_qty){
+        this.product_name = product_name;
+        this.product_description = product_description;
+        this.url_image = url_image;
+        this.product_price = product_price;    
+        this.for_cat = for_cat;
+        this.for_dog = for_dog;
+        this.id_product = id_product;
+        this.product_qty = product_qty;
     }
 }
 
@@ -25,6 +26,8 @@ boton.addEventListener("click", function enviar(e){
     let gato = document.getElementById("gato").checked;
     let perro = document.getElementById("perro").checked;
     let formulario = document.getElementById("form");
+    let id = 38;
+    let cantidad = 10;
 
     if(nombre == "" || descripcion == "" || precio == "" || imagen == ""){
         alert("All fields are mandatory");
@@ -34,23 +37,42 @@ boton.addEventListener("click", function enviar(e){
         } else {
 
     let articulo = new Articulo(nombre,descripcion,imagen,precio,
-        [document.getElementById("HyW").checked,
-         document.getElementById("TyC").checked,
-         document.getElementById("Home").checked,
-         document.getElementById("SandA").checked,
-         document.getElementById("TandT").checked,
-         document.getElementById("Toys").checked,
-         document.getElementById("Grooming").checked,
-         document.getElementById("Dental").checked,
-         document.getElementById("NandC").checked,
-         document.getElementById("DryFood").checked,
-         document.getElementById("WetFood").checked,
-         ], gato, perro);
+        // [document.getElementById("HyW").checked,
+        //  document.getElementById("TyC").checked,
+        //  document.getElementById("Home").checked,
+        //  document.getElementById("SandA").checked,
+        //  document.getElementById("TandT").checked,
+        //  document.getElementById("Toys").checked,
+        //  document.getElementById("Grooming").checked,
+        //  document.getElementById("Dental").checked,
+        //  document.getElementById("NandC").checked,
+        //  document.getElementById("DryFood").checked,
+        //  document.getElementById("WetFood").checked,
+        //  ],
+        gato, perro, id, cantidad);
 
-    articulosArray.push(articulo);
-    let articuloJson = JSON.stringify(articulosArray);
+         let productJson = JSON.stringify(articulo);
 
-    localStorage.setItem("articulos", articuloJson);
+         fetch('http://localhost:8080/products/newProduct', {  
+            method: 'POST',
+            body: productJson,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data);
+                    
+            })
+            .catch((error) => {
+                console.error("error",error);
+            });
+
+    // articulosArray.push(articulo);
+    // let articuloJson = JSON.stringify(articulosArray);
+
+    // localStorage.setItem("articulos", articuloJson);
     alert("New item created succesfully");
     formulario.reset();
         }
@@ -59,32 +81,44 @@ boton.addEventListener("click", function enviar(e){
 
 botonMostrar.addEventListener("click", function mostrar(e){
     e.preventDefault();
-     function addItem(item){
-        const itemHTML = '<div class="card">\n' +
-            '    <img src="'+item.img +'" class="card-img-top" alt="image">\n' +
-            '    <div class="card-body">\n' +
-            '        <h5 class="card-title formato-letras">'+item.name+'</h5>\n' +
-            '        <p class="card-text formato-letras">'+item.description+'</p>\n' +
-            '        <p class="card-text formato-letras">'+item.prize+'</p>\n' +
-            '        <a href="#" class="btn btn-primary">Add</a>\n' +
-            '    </div>\n' +
-            '</div>\n' +
-            '<br/>';
-        const itemsContainer = document.getElementById("list-items");
-        itemsContainer.innerHTML += itemHTML;
-    }
 
-    let articulosObjeto = JSON.parse(localStorage.getItem("articulos"));
-    console.log(articulosObjeto);
+    fetch('http://localhost:8080/products/display', {  
+            method: 'GET',
+            })
+                .then(res => res.json())
+                .then(data => { 
+                    console.log(data);
+            })
+            .catch((error) => {
+                console.error("error",error);
+                console.log("fallo :(");
+            });
+    //  function addItem(item){
+    //     const itemHTML = '<div class="card">\n' +
+    //         '    <img src="'+item.img +'" class="card-img-top" alt="image">\n' +
+    //         '    <div class="card-body">\n' +
+    //         '        <h5 class="card-title formato-letras">'+item.name+'</h5>\n' +
+    //         '        <p class="card-text formato-letras">'+item.description+'</p>\n' +
+    //         '        <p class="card-text formato-letras">'+item.prize+'</p>\n' +
+    //         '        <a href="#" class="btn btn-primary">Add</a>\n' +
+    //         '    </div>\n' +
+    //         '</div>\n' +
+    //         '<br/>';
+    //     const itemsContainer = document.getElementById("list-items");
+    //     itemsContainer.innerHTML += itemHTML;
+    // }
 
-    let i=0;
+    // let articulosObjeto = JSON.parse(localStorage.getItem("articulos"));
+    // console.log(articulosObjeto);
+
+    // let i=0;
     
-    while(articulosObjeto[i] != undefined){
-        addItem({'name':articulosObjeto[i].nombre,
-        'img': articulosObjeto[i].imagen,
-        'description':articulosObjeto[i].descripcion,
-        'prize':articulosObjeto[i].precio});
+    // while(articulosObjeto[i] != undefined){
+    //     addItem({'name':articulosObjeto[i].nombre,
+    //     'img': articulosObjeto[i].imagen,
+    //     'description':articulosObjeto[i].descripcion,
+    //     'prize':articulosObjeto[i].precio});
 
-        i++
-    }
+    //     i++
+    // }
 })
